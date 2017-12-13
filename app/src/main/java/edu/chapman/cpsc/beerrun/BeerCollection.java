056a2da22f5154672a6a5cf824f2f549a6f969f4
ryanburns31892 <rburns@chapman.edu>
2017-12-12 22:30:12 -0800

    bit of refactoring to demonstrate async stuff

--- app/src/main/java/edu/chapman/cpsc/beerrun/BeerCollection.java
+++ app/src/main/java/edu/chapman/cpsc/beerrun/BeerCollection.java
@@ -21,7 +21,7 @@ import static android.support.test.InstrumentationRegistry.getContext;
 
 public class BeerCollection {
     private static final String TAG = "BeerCollection";
-    private static final String API_KEY = "0e62b079fc775a1afe28e07ac58239fb";
+    public static final String API_KEY = "0e62b079fc775a1afe28e07ac58239fb";
 
     private static BeerCollection collection;
 
@@ -33,40 +33,13 @@ public class BeerCollection {
     }
 
     private List<BeerModel> beers;
-    private List<BeerResponse.DataBean> beerResponse;
 
     private BeerCollection(){
         this.beers = new ArrayList<>();
-        this.beerResponse = new ArrayList<>();
+    }
 
-        ApiClient apiClient = new ApiClient();
-        ApiInterface apiService = apiClient.getClient().create(ApiInterface.class);
-
-        Call<BeerResponse> call = apiService.getAllBeers(API_KEY);
-        call.enqueue(new Callback<BeerResponse>() {
-            @Override
-            public void onResponse(Call<BeerResponse> call, Response<BeerResponse> response) {
-                if(response.isSuccessful()) {
-                    beerResponse = response.body().getData();
-                }
-                else{
-                    try{
-                        JSONObject jObjError = new JSONObject(response.errorBody().string());
-                        Toast.makeText(getContext(), jObjError.getString("message"),
-                                Toast.LENGTH_LONG).show();
-                    } catch (Exception e){
-                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<BeerResponse> call, Throwable t) {
-                Log.e(TAG, t.toString());
-
-            }
-        });
-
+    public void setBeers(List<BeerResponse.DataBean> beerResponse)
+    {
         for(int i = 0; i < beerResponse.size(); ++i){
             Log.i("TEST", "Generating instances of beers");
 
@@ -76,7 +49,7 @@ public class BeerCollection {
             beer.setDescription(beerResponse.get(i).getDescription());
             beer.setAbv(beerResponse.get(i).getAbv());
             beer.setIbu(beerResponse.get(i).getIbu());
-            beer.setBrewery(beerResponse.get(i).getBreweries().get(i).getName());
+            beer.setBrewery("test");
 
             if(beerResponse.get(i).getLabels() != null) {
                 beer.setUrl(beerResponse.get(i).getLabels().getMedium());
--- app/src/main/java/edu/chapman/cpsc/beerrun/fragments/BeerFragment.java
+++ app/src/main/java/edu/chapman/cpsc/beerrun/fragments/BeerFragment.java
@@ -1,18 +1,32 @@
 package edu.chapman.cpsc.beerrun.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import edu.chapman.cpsc.beerrun.BeerCollection;
 import edu.chapman.cpsc.beerrun.R;
 import edu.chapman.cpsc.beerrun.adapters.BeerAdapter;
+import edu.chapman.cpsc.beerrun.models.BeerResponse;
+import edu.chapman.cpsc.beerrun.services.ApiClient;
+import edu.chapman.cpsc.beerrun.services.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.support.test.InstrumentationRegistry.getContext;
 
 /**
  *
@@ -20,6 +34,8 @@ import edu.chapman.cpsc.beerrun.adapters.BeerAdapter;
  */
 
 public class BeerFragment extends Fragment {
+
+    private final String TAG = "BeerFragment";
     private BeerAdapter mAdapter;
 
     @Nullable
@@ -45,6 +61,34 @@ public class BeerFragment extends Fragment {
     @Override
     public void onResume(){
         super.onResume();
-        this.mAdapter.notifyDataSetChanged();
+
+        ApiClient apiClient = new ApiClient();
+        ApiInterface apiService = apiClient.getClient().create(ApiInterface.class);
+
+        Call<BeerResponse> call = apiService.getAllBeers(BeerCollection.API_KEY);
+        call.enqueue(new Callback<BeerResponse>() {
+            @Override
+            public void onResponse(Call<BeerResponse> call, Response<BeerResponse> response) {
+                if(response.isSuccessful()) {
+                    BeerCollection.GetInstance().setBeers(response.body().getData());
+                    mAdapter.notifyDataSetChanged();
+                }
+                else{
+                    try{
+                        JSONObject jObjError = new JSONObject(response.errorBody().string());
+                        Toast.makeText(getContext(), jObjError.getString("message"),
+                                Toast.LENGTH_LONG).show();
+                    } catch (Exception e){
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BeerResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+
+            }
+        });
     }
 }
